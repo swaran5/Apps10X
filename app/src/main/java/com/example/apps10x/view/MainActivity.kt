512953ecb637979e1f0fc067d821ee0b4b331f6a
swaran5 <73553856+swaran5@users.noreply.github.com>
2022-02-19 18:34:26 +0530

    api call inside coroutine scope

--- app/src/main/java/com/example/apps10x/view/MainActivity.kt
+++ app/src/main/java/com/example/apps10x/view/MainActivity.kt
@@ -3,27 +3,63 @@ package com.example.apps10x.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.apps10x.R
+import com.example.apps10x.databinding.ActivityMainBinding
 import com.example.apps10x.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    val context = this
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        val viewModel: MainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.isLoading = true
 
+        CoroutineScope(Main).launch {
+            print("inside launch")
+
+            viewModel.getWeather().observe(context, {
+
+                animateForecastLayout()
+                binding.temperature.text = kelvinToCelsius(it.main.temp)
+                binding.isLoading = false
+
+            })
+
+        }
+
+    }
+
+    private fun animateForecastLayout() {
         forecast_layout.startAnimation(AnimationUtils.loadAnimation(
             applicationContext,
             R.anim.move_up
         ))
-
-        viewModel.getWeather().observe(this, {
-            val temp = it.main.temp
-            val tem = it.main.feelsLike
-        })
-
     }
+
+    private fun kelvinToCelsius(temp: Double): String {
+        val stringTemp = round(temp.toInt() - 273.15, 1)
+        return "$stringTemp°C"
+    }
+
+    private fun round(value: Double, precision: Int): Double {
+        val scale = 10.0.pow(precision.toDouble()).toInt()
+        return (value * scale).roundToInt().toDouble() / scale
+    }
+
 }
